0b7db10cfe1d02cc18ba3cc0ee69cba7dae58993
Nishikant Kumar <nishikantji392@gmail.com>
2025-11-27 23:59:28 +0530

    updated serverport

--- backend/src/main/java/com/healthcare/appointment/HealthcareAppointmentApplication.java
+++ backend/src/main/java/com/healthcare/appointment/HealthcareAppointmentApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableScheduling
+@SpringBootApplication
 public class HealthcareAppointmentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HealthcareAppointmentApplication.class, args);
     }
 }
+
+
 
